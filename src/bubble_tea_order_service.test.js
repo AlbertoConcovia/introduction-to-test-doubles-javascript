edf02e9ba27f99b595c7065eb8d50986946fdd03
albertoconcovia <concovia@gmail.com>
2021-10-18 23:56:35 +0100

    Activity 5 - test parameterisation added in bubble_tea_roulette_service.test.js

--- src/bubble_tea_order_service.test.js
+++ src/bubble_tea_order_service.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable linebreak-style */
 const {createOrderRequest} = require('./bubble_tea_order_service');
 const bubbleTeaType = require('./bubble_tea_type');
@@ -45,26 +46,49 @@ test('test successful bubble tea order request', () => {
 });
 
 
-test('test successful bubble tea order request - type validation', () => {
-  // Arrange
-  const bubbleTeaRequest = {
-    'paymentDetails': dummyPaymentDetails,
-    'bubbleTea': {
-      'type': bubbleTeaType.OOLONGMILKTEA,
-    },
-  };
+// const add = (a, b) => a + b;
+// const cases = [[2, 2, 4], [-2, -2, -4], [2, -2, 0]];
+// describe('Test.each - bubble_tea_order_service', () => {
+//   test.each(cases)(
+//       'given %p and %p as arguments, returns %p',
+//       (firstArg, secondArg, expectedResult) => {
+//         const result = add(firstArg, secondArg);
+//         expect(result).toEqual(expectedResult);
+//       },
+//   );
+// });
 
-  // Act
-  const orderRequest = createOrderRequest(bubbleTeaRequest);
+const casesTeaType = [
+  'OOLONGMILKTEA',
+  'JASMINEMILKTEA',
+  'MATCHAMILKTEA',
+  'PEACHICETEA',
+  'LYCHEEICETEA',
+];
 
-  // Assert
-  expect(orderRequest.name).toBe(dummyPaymentDetails.name);
-  expect(orderRequest.digits).toBe(dummyPaymentDetails.debitCard.digits);
-  expect(orderRequest.type).toBe(bubbleTeaRequest.bubbleTea.type);
-  expect(
-      bubbleTeaMessenger.sendBubbleTeaOrderRequestEmail,
-  ).toHaveBeenCalledWith(orderRequest);
-  expect(
-      bubbleTeaMessenger.sendBubbleTeaOrderRequestEmail,
-  ).toHaveBeenCalledTimes(1);
+describe('Test.each successful bubble_tea_order_service', () => {
+  test.each(casesTeaType)(
+      `Given ${casesTeaType} type validation - then ${casesTeaType}`, () => {
+        // Arrange
+        const bubbleTeaRequest = {
+          'paymentDetails': dummyPaymentDetails,
+          'bubbleTea': {
+            'type': casesTeaType,
+          },
+        };
+
+        // Act
+        const orderRequest = createOrderRequest(bubbleTeaRequest);
+
+        // Assert
+        expect(orderRequest.name).toBe(dummyPaymentDetails.name);
+        expect(orderRequest.digits).toBe(dummyPaymentDetails.debitCard.digits);
+        expect(orderRequest.type).toBe(bubbleTeaRequest.bubbleTea.type);
+        expect(
+            bubbleTeaMessenger.sendBubbleTeaOrderRequestEmail,
+        ).toHaveBeenCalledWith(orderRequest);
+        expect(
+            bubbleTeaMessenger.sendBubbleTeaOrderRequestEmail,
+        ).toHaveBeenCalledTimes(1);
+      });
 });
--- src/bubble_tea_roulette_service.test.js
+++ src/bubble_tea_roulette_service.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 const getRandomBubbleTeaType = require('./bubble_tea_roulette_service');
 
 test('should generate random bubble tea', () => {
@@ -10,6 +11,20 @@ test('should generate random bubble tea', () => {
 
   // Assert
   expect(bubbleTea).toBe('JASMINEMILKTEA');
+
+  // Restore the default Math.random
+  jest.spyOn(global.Math, 'random').mockRestore();
+});
+
+test('Given random number is 0.4 Then MATCHAMILKTEA', () => {
+  // Arrange
+  jest.spyOn(global.Math, 'random').mockReturnValue(0.4);
+
+  // Act
+  const bubbleTea = getRandomBubbleTeaType();
+
+  // Assert
+  expect(bubbleTea).toBe('MATCHAMILKTEA');
 
   // Restore the default Math.random
   jest.spyOn(global.Math, 'random').mockRestore();
