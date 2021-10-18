3eac52add0f06e3f608c36a2dd1e14fb2a8bb778
albertoconcovia <concovia@gmail.com>
2021-10-18 21:55:12 +0100

    Exercise 2.1 - completed

--- src/bubble_tea_order_service.test.js
+++ src/bubble_tea_order_service.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 const {createOrderRequest} = require('./bubble_tea_order_service');
 const bubbleTeaType = require('./bubble_tea_type');
 const bubbleTeaMessenger = require('./bubble_tea_messenger');
@@ -35,8 +36,34 @@ test('test successful bubble tea order request', () => {
   // Assert
   expect(orderRequest.name).toBe(dummyPaymentDetails.name);
   expect(orderRequest.digits).toBe(dummyPaymentDetails.debitCard.digits);
-  expect(bubbleTeaMessenger.sendBubbleTeaOrderRequestEmail)
-      .toHaveBeenCalledWith(orderRequest);
-  expect(bubbleTeaMessenger.sendBubbleTeaOrderRequestEmail)
-      .toHaveBeenCalledTimes(1);
+  expect(
+      bubbleTeaMessenger.sendBubbleTeaOrderRequestEmail,
+  ).toHaveBeenCalledWith(orderRequest);
+  expect(
+      bubbleTeaMessenger.sendBubbleTeaOrderRequestEmail,
+  ).toHaveBeenCalledTimes(1);
+});
+
+test('test successful bubble tea order request - type validation', () => {
+  // Arrange
+  const bubbleTeaRequest = {
+    paymentDetails: dummyPaymentDetails,
+    bubbleTea: {
+      type: bubbleTeaType.OOLONGMILKTEA,
+    },
+  };
+
+  // Act
+  const orderRequest = createOrderRequest(bubbleTeaRequest);
+
+  // Assert
+  expect(orderRequest.name).toBe(dummyPaymentDetails.name);
+  expect(orderRequest.digits).toBe(dummyPaymentDetails.debitCard.digits);
+  expect(orderRequest.type).toBe(bubbleTeaRequest.bubbleTea.type);
+  expect(
+      bubbleTeaMessenger.sendBubbleTeaOrderRequestEmail,
+  ).toHaveBeenCalledWith(orderRequest);
+  expect(
+      bubbleTeaMessenger.sendBubbleTeaOrderRequestEmail,
+  ).toHaveBeenCalledTimes(1);
 });
